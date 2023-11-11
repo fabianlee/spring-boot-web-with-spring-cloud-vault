b72389e6b13ecb07ed00431deb268f1499cf9c97
Fabian Lee <flee999@hotmail.com>
2023-11-10 23:03:39 -0500

    changes for new tag v1.0.13

--- src/main/java/org/fabianlee/springbootwithvaultsidecar/GreetingController.java
+++ src/main/java/org/fabianlee/springbootwithvaultsidecar/GreetingController.java
@@ -1,6 +1,7 @@
 package org.fabianlee.springbootwithvaultsidecar;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,8 @@ public class GreetingController {
 	public String secret() {
 		return "foo/username/password: " + getVaultSecretsFromEnv();
 	}
-	
+
+	@Lazy
 	@Autowired Environment env;
 	public String getVaultSecretsFromEnv() {
 		return env.getProperty("foo") + "," + env.getProperty("username") + "," + env.getProperty("password");
